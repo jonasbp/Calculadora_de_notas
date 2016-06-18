0b67265e2bde670fa1350ceeffb5332865ea61b8
jonasbp <jonas-bonfa@hotmail.com>
2016-06-18 10:30:19 -0300

    First Commit

---
+++ seb1.py
@@ -0,0 +1,31 @@
+# Programa para calcular nota de prova
+# Comecou dia 18 de junho de 2016 as 08 horas da manha
+# Feito por Jonas Bonfa - jonas-bonfa@hotmail.com
+# Calculador para : Historia/Geografia , Matematica , Fisica
+
+#variaveis
+
+print(" ")
+print ("---------------------------")
+print ("--- Calculador de notas ---")
+print ("---------------------------")
+aberta = float(raw_input('Nota total da prova aberta (max 10.00) : '))
+teste = float(raw_input('Nota total dos teste (max 10.00) : '))
+print ("---------------------------")
+print ('Calculando...')
+print ("---------------------------")
+#calculador
+soma = (aberta) + (teste)
+final = soma / 2
+if final > 10 :
+    print ("---------------------------")
+    print('Confira suas notas,alguma esta errada!')
+    print ("---------------------------")
+else :
+
+    print ("---------------------------")
+    print('Sua nota no bimestre e:')
+    print ("---------------------------")
+    if final == 10 :
+        print ('Parabens voce atingiu a nota maxima! ;)')
+    print final
